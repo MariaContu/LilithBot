725faf95801be3c5cf7a144bee0454c1b9c15ac3
MariaContu <mariaescontu@gmail.com>
2023-07-27 15:26:16 -0300

    back to discord.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import nextcord
-from nextcord.ext import commands
+import discord
+from discord.ext import commands
 from dice import d4, d6, d8, d10, d12, d20, d100
 from myToken import token
 
-intents = nextcord.Intents.default()
+intents = discord.Intents.default()
 intents.message_content = True
 
 client = commands.Bot(command_prefix='!',intents=intents)
 
+dados_disponiveis = ['d4', 'd6', 'd8', 'd10', 'd12', 'd20', 'd100']
 
 @client.event
 async def on_ready():
@@ -20,13 +21,18 @@ async def oi(ctx):
     await ctx.send("Oi tudo bem? Sou um bot para rpg :)")
 
 
-testServerId=1133139602992017409    
-@client.slash_command(name = "rolar", description="Rolagem de d4, d6, d8, d12, d20 e d100", guild_ids=[testServerId])    
-async def roll(interaction: nextcord.Interaction, dado:str):    
+testServerId=1133139602992017409  
+
+@client.tree.command(name = "rolar", description="Rolagem de d4, d6, d8, d12, d20 e d100")    
+async def roll(interaction: discord.Interaction, dado:str):  
     
     quantidade, dTipo = dado.split('d')
+    if not quantidade: quantidade = "1"
+    
     quantidade = int(quantidade)
     dTipo = "d" + dTipo
+    
+    if dTipo not in dados_disponiveis: await interaction.response.send_message(f"Dado {dTipo} não está entre os dados disponívels, por favor insira um valor válido.")
     
     resultado = None
     if dTipo == "d4":
@@ -44,7 +50,7 @@ async def roll(interaction: nextcord.Interaction, dado:str):
     elif dTipo == "d100":
         resultado = d100()
     
-    await interaction.response.send_message(f"{interaction.user.mention} :game_die: Rolou um {dado} e tirou {resultado}")
+    await interaction.response.send_message(f"{interaction.user.mention} :game_die: Rolou {quantidade}{dTipo} e tirou {resultado}")
 
 
 ##    
