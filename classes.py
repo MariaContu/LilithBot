5851a480decf2b000e2df8a21d7d096eaa6204a8
MariaContu <mariaescontu@gmail.com>
2023-08-06 15:50:42 -0300

    classes

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Classes():
+    def __init__(self,nome,pv,pe,ps):
+        self.nome=nome
+        self.pv = pv
+        self.pe=pe
+        self.ps=ps
+        
+
+Combatente = Classes("Combatente",20,2,12)
+Especialista=Classes("Especialista",16,3,16)
+Ocultista=Classes("Ocultista",12,4,20)
